e98a921becb7207219ec5f8cfd8f08a8979a45ae
moose <moosecodes@gmail.com>
2023-06-10 12:50:04 -0700

    update server config script

--- serverConfig.sh
+++ serverConfig.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-cd ~
-mkdir ~/api
-cd ~/api
-tzselect
+#tzselect
+mkdir -p /var/moosecodes/api && cd $_
 apt update -y && apt upgrade -y && apt install -y php-curl php-xml docker.io docker-compose composer php8.1-xdebug nodejs npm
 git clone https://github.com/moosecodes/news-app.git .
 composer update
